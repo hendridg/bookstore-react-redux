05fc57a84fda600924ad697351b9b54797e33505
hendridg <hendridg@gmail.com>
2022-02-21 15:40:41 -0400

    Update App.js file with router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello App!</h2>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/*" element={<div>Dont exist</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
