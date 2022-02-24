c9f2c0038959a7c098f249682c7af0df7e7e90ce
hendridg <hendridg@gmail.com>
2022-02-24 10:12:47 -0400

    Update App.js file, delete import App.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
@@ -6,10 +5,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)', height: '100%' }}
-    >
+    <div>
       <BrowserRouter>
         <Header />
         <Routes>
